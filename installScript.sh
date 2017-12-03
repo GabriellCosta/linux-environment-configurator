db689a7f9857ee25874665ad2a73f0e7e256c9f9
Gabriel <gabrielcosta08@gmail.com>
2017-12-03 16:29:08 -0200

    Add xclip

--- installScript.sh
+++ installScript.sh
@@ -3,5 +3,6 @@ apt-get install zsh
 apt-get -y install git
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 chsh -s /bin/zsh
+apt-get install xclip
 echo "TODO: zsh-syntax-highlighting,zsh-autosuggestions"
 echo "git alias"
