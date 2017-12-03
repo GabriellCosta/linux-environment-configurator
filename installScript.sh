2c60bdf18a85688654ea76b858dbc5ccd75cecdd
Gabriel <gabrielcosta08@gmail.com>
2017-12-03 16:21:53 -0200

    Initial commit

---
+++ installScript.sh
@@ -0,0 +1,7 @@
+apt-get install vim
+apt-get install zsh
+apt-get -y install git
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+chsh -s /bin/zsh
+echo "TODO: zsh-syntax-highlighting,zsh-autosuggestions"
+echo "git alias"
