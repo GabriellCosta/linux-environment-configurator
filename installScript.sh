c0af1ad66dfb694e08dd591086324f7e830124ca
Gabriel Costa <gabrielcosta08@gmail.com>
2017-12-06 07:59:18 -0200

    Update installScript.sh
    Use zshInstallScript

--- installScript.sh
+++ installScript.sh
@@ -1,8 +1,6 @@
 apt-get install vim
-apt-get install zsh
 apt-get -y install git
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-chsh -s /bin/zsh
+./zshInstallScript.sh
 apt-get install xclip
 echo "TODO: zsh-syntax-highlighting,zsh-autosuggestions"
 echo "git alias"
